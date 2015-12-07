bc51f26c5c47f7ad06eeef5208305a263f7ba539
Cyril Vincent <contact@cyrilvincent>
2015-12-07 09:47:22 +0100

    g

--- FormationConsole/Program.cs
+++ FormationConsole/Program.cs
@@ -11,7 +11,7 @@ namespace FormationConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world"); 
-            Console.ReadKey();
+            Console.ReadKey(); 
         }
     }
 }
