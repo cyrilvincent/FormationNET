5256dad617264e8c07f6da9baefbeeac5568f754
Cyril Vincent <contact@cyrilvincent>
2015-12-07 09:46:27 +0100

    commit 2

--- FormationConsole/Program.cs
+++ FormationConsole/Program.cs
@@ -10,7 +10,7 @@ namespace FormationConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world"); 
             Console.ReadKey();
         }
     }
