29072f0688fad40af539bb78b6740a020ff66c3c
cyrilvincent <contact@cyrilvincent.com>
2020-12-14 11:04:31 +0100

    DSP

--- FormationOO/FormationOO/Program.cs
+++ FormationOO/FormationOO/Program.cs
@@ -28,8 +28,12 @@ namespace FormationOO
             {
                 Console.WriteLine("Impair");
             }
-            int[] tab = { 1, 2, 3, 4 };
+            int[] tab = { 1, 99, 3, 4 };
             foreach(int i in tab)
+            {
+                Console.WriteLine(i);
+            }
+            for(int i=0;i<10;i++)
             {
                 Console.WriteLine(i);
             }
@@ -37,5 +41,13 @@ namespace FormationOO
             Console.WriteLine(res);
             Console.ReadKey();
         }
+
+        /*TP1
+         *Faire la fonction IsEven(int x) qui retourne true si x est pair false sinon
+         *Saisir un chiffre et vérifier s'il est pair ou non
+         *Faire une fonction DisplayFor(int nb) qui affiche une boucle de 0 à <nb
+         *Faire la fonction IsPrime(int nb) qui retourne true si nombre premier
+         *  Tout nombre >= 2 est premier sauf s'il possède un diviseur compris entre 2 et n-1
+         */
     }
 }
