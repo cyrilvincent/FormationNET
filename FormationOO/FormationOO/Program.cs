631de4f103d94d08e3af38456b3cbd72355d764f
cyrilvincent <contact@cyrilvincent.com>
2020-12-14 10:55:35 +0100

    ATP

--- FormationOO/FormationOO/Program.cs
+++ FormationOO/FormationOO/Program.cs
@@ -8,9 +8,33 @@ namespace FormationOO
 {
     class Program
     {
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("Saisir un entier: ");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+            int nb = int.Parse(s);
+            if(nb % 2 == 0)
+            {
+                Console.WriteLine("Pair");
+            }
+            else
+            {
+                Console.WriteLine("Impair");
+            }
+            int[] tab = { 1, 2, 3, 4 };
+            foreach(int i in tab)
+            {
+                Console.WriteLine(i);
+            }
+            int res = Add(3, 2);
+            Console.WriteLine(res);
             Console.ReadKey();
         }
     }
